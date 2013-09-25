6be4062115d1fc86cf46c4432e9c79243351f1c5
Diogo Tridapalli <diogo@diogot.com>
2013-09-24 23:08:54 -0300

    Initial Commit

---
+++ DynamicsExamples/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  DynamicsExamples
+//
+//  Created by Diogo Tridapalli on 9/24/13.
+//  Copyright (c) 2013 Diogo Tridapalli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicsExamples/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DynamicsExamples
+//
+//  Created by Diogo Tridapalli on 9/24/13.
+//  Copyright (c) 2013 Diogo Tridapalli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTAppDelegate class]));
+    }
+}
